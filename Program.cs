f6ecbdddff4e7b6059697460a7a0f97c0cd393e8
limorYavetz <anayavetz22@gmail.com>
2024-12-31 12:29:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rediscsharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ resp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace rediscsharp
+{
+    const string _DELIM = "\r\n";
+    enum RespId : char { STRING='+',ERR='-',INT=':',BULK='$',ARR='*'};
+    public class Srlz
+    {
+        public Srlz()
+        {
+        }
+    }
+}
+
