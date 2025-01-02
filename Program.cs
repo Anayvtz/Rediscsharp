47c55b8960e0037d10e05ead5d116cbdecdac21d
limorYavetz <anayavetz22@gmail.com>
2025-01-02 12:44:03 +0200

    remove program cant find how to delete file

--- Program.cs
+++ Program.cs
@@ -9,17 +9,6 @@ namespace rediscsharp
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("hello world");
-            (string? str,string remains)=Dsrlz.dsrlz_str("$3\r\nlen\r\n");
-            Console.WriteLine($"str is {str} and remain is {remains}");
-            (List<string>? arr, string remainl) = Dsrlz.dsrlz_arr("*2\r\n:23\r\n$3\r\nyou\r\n");
-            Console.WriteLine($"remainl is {remainl}");
-            for (int i = 0; arr != null && i < arr?.Count; ++i)
-            {
-                Console.WriteLine($"list item {i} is {arr[i]}");
-            }
-        }
+       
     }
 }
